16b6ae5e341a2e6386ec1009eed9a33219e1faeb
sakahiro <sakahiro18@docomo.ne.jp>
2020-03-24 01:26:27 +0900

    C#スクリプトの追加

---
+++ BulletController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject explosionPrefab;      //爆発エフェクトのPrefab
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0.2f, 0);
+
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //衝突したときにスコアを更新する
+        GameObject.Find("Canvas").GetComponent<UIController>().AddScore();
+
+        //爆発エフェクトを生成
+        GameObject effect = Instantiate(explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+        Destroy(effect, 1.0f);
+
+        Destroy(coll.gameObject);
+        Destroy(gameObject);
+    }
+}
